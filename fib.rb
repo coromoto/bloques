3ff2bcde958b17a8d34279fdc647987b3a504705
Coromoto Leon <cleon@ull.es>
2017-11-21 09:22:35 +0000

    memoization

--- fib.rb
+++ fib.rb
@@ -1,3 +1,7 @@
+require 'benchmark'
+include Benchmark
+
+
 #paso de parametros a un bloque
 
 def fib_up_to(max)
@@ -8,4 +12,36 @@ def fib_up_to(max)
   end
 end
 
-fib_up_to(1000) {|f| print f, " "}
+#fib_up_to(1000) {|f| print f, " "}
+
+
+def fib_recursivo(n)
+  return n if n < 2
+  fib_recursivo(n-1) + fib_recursivo(n-2)
+end
+
+#puts (1..5).map { |i| fib_recursivo(i)}
+
+
+@cache = [0,1]
+
+
+def fib_memoization(n)
+   return @cache[n] if @cache[n] 
+   @cache[n] = fib_memoization(n-1) + fib_memoization(n-2)
+end
+
+
+#puts (1..5).map { |i| fib_memoization(i)}
+
+
+
+
+Benchmark.benchmark(CAPTION, 7, FORMAT, ">total:", ">avg:") do |x|
+  tf = x.report("recursivo")   { (1..30).map{ |i| fib_recursivo(i)}  }
+  tf = x.report("memoizado")   { (1..30).map{ |i| fib_memoization(i)}  }
+end
+
+
+
+
